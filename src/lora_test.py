569684a825f1047c96fce3020c09c1f33f307882
saadyaq <saadyaqine91@gmail.com>
2025-10-27 19:41:49 +0100

    Testing Lora

--- src/lora_test.py
+++ src/lora_test.py
@@ -19,8 +19,8 @@ def generate_answer(question,context):
     Contexte: {context}
     Réponse:"""
     inputs=tokenizer(prompt,return_tensors="pt").to(model.device)
-    outputs=model.generate(**inputs,max_new_tokens=256
-                           ,do_smple=True,
+    outputs=model.generate(**inputs,max_new_tokens=256,
+                           do_sample=True,
                            temperature=0.7,
                            pad_token_id=tokenizer.eos_token_id)
     return tokenizer.decode(outputs[0],skip_special_tokens=True)
